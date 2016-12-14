aa9c2ccc7a7489a34c480324eeba303755388c39
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2016-12-14 18:54:17 +0000

    Add plain prototype

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ import path from 'path';
 
 const app = express();
 
+// Serve the prototypes from a static folder
+app.use(express.static('prototypes'));
 
 app.get('/', (req, res) => res.sendFile(path.join(`${__dirname}/index.html`)));
 
