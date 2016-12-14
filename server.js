45f5446ebfb4ee1cb35a337e1282af143d3af1aa
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2016-12-14 18:54:06 +0000

    Update server.js to ES6 and linted,

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 // @flow
 
-var express = require('express')
-var app = express()
+import express from 'express';
+import path from 'path';
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+const app = express();
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+
+app.get('/', (req, res) => res.sendFile(path.join(`${__dirname}/index.html`)));
+
+app.listen(3000);
