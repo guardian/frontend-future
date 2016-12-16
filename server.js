b9f0fba434760050564d824d17eaed94d61c6cb1
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2016-12-16 14:31:59 +0000

    Update server to use handlebars engine for plain

--- server.js
+++ server.js
@@ -2,12 +2,20 @@
 
 import express from 'express';
 import path from 'path';
+import hbs from 'hbs';
 
 const app = express();
 
-// Serve the prototypes from a static folder
-app.use(express.static('prototypes'));
+app.set('view engine', 'hbs');
+
+// Serve the prototypes from a view
+app.set('views', path.join(__dirname, '/prototypes'));
+hbs.registerPartials(`${__dirname}/prototypes/plain/partials/`);
 
 app.get('/', (req, res) => res.sendFile(path.join(`${__dirname}/index.html`)));
+
+app.get('/plain', (req, res) => {
+  res.render('plain/index');
+});
 
 app.listen(3000);
