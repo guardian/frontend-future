76a703586c57ae7da1bf538f1630dea6a99e2584
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2017-01-04 17:48:53 +0000

    Move data and import from outside server.js

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ import hbs from 'hbs';
 import fs from 'fs';
 import svelteApp from './prototypes/svelte/dist/app-ssr';
 
+import cardData from './prototypes/data';
+
 const app = express();
+
 
 app.set('view engine', 'hbs');
 
@@ -21,24 +24,7 @@ app.get('/plain', (req, res) => res.render('plain/index'));
 app.get('/svelte', (req, res) => {
   const template = fs.readFileSync(path.join(__dirname, 'prototypes/svelte/index.html'), 'utf-8');
 
-  const html = svelteApp.render({
-    cardData: [
-      {
-        kicker: 'Yahoo hack',
-        headline: '1bn accounts compromised by biggest data breach in history',
-        standfirst: 'The latest incident to emerge – which happened in 2013 – is probably distinct from the breach of 500m user accounts in 2014',
-        tone: 'opinion',
-        showKicker: false,
-      },
-      {
-        kicker: 'Gareth hack',
-        headline: '1 account compromised by smallest data breach in history',
-        standfirst: 'The latest incident to emerge – which happened in 2016',
-        tone: 'news--item',
-        showKicker: true,
-      },
-    ],
-  });
+  const html = svelteApp.render(cardData);
   const { css } = svelteApp.renderCss();
 
   res.end(template.replace('/* CSS */', css).replace('<!-- HTML -->', html));
