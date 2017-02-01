86166898088d98773af3a43d461512305df7a68c
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2017-02-01 12:25:19 +0000

    Remove unused files

--- prototypes/preact-styled-components/template.js
+++ prototypes/preact-styled-components/template.js
@@ -6,7 +6,6 @@ export default ({ body, title, styles }) => `
       <link rel="stylesheet" href="http://pasteup.guim.co.uk/0.0.5/css/fonts.pasteup.min.css">
       <link rel="stylesheet" href="/normalize.css">
       <link rel="stylesheet" href="/base-styles.css">
-      <link rel="stylesheet" href="/preact/dist/assets/style.css">
       <style>
         ${styles}
       </style>
