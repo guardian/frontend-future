3875dacb2bff5f55d267d91e1ba8ea1e7492481b
sndrs <a@snd.rs>
2017-05-03 16:40:41 +0100

    test server and client together

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,5 +1,16 @@
 import index from './index';
 
+document.body.innerHTML = `
+    <style class="_styletron_hydrate_"></style>
+    <div id="app"></div>
+`;
+
 it('renders correctly on the server', () => {
+    expect(index()).toMatchSnapshot();
+});
+
+it('renders correctly in the browser', () => {
+    global.BROWSER = true;
+    global.SERVER = false;
     expect(index()).toMatchSnapshot();
 });
