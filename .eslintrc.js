a6d437aac78effd21ca35a3dd245dfedce3bac1d
sndrs <a@snd.rs>
2017-06-16 13:57:01 +0100

    dont’ pass flow errors to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     parser: 'babel-eslint',
     extends: ['airbnb', 'prettier', 'prettier/flowtype', 'prettier/react'],
     parserOptions: { sourceType: 'module', ecmaVersion: 2017, jsx: true },
-    plugins: ['flowtype-errors', 'flow-header', 'jest', 'prettier'],
+    plugins: ['flow-header', 'jest', 'prettier'],
     env: { browser: true, node: true, es6: true, 'jest/globals': true },
     globals: { el: true, render: true, BROWSER: true, SERVER: true },
     settings: { react: { pragma: 'h' }, 'import/resolver': 'webpack' },
@@ -18,8 +18,6 @@ module.exports = {
                 jsxBracketSameLine: true,
             },
         ],
-
-        'flowtype-errors/show-errors': 'error',
         'flow-header/flow-header': 'error',
 
         'react/react-in-jsx-scope': 'off',
